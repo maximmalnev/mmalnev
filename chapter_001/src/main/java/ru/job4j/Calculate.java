c47e9273d4e3fdcf48d29b86c3ef734b7ef839f3
Maxim Malnev <mazular@ya.ru>
2017-07-12 23:31:44 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,13 +1,17 @@
-/**
-* Package for calculate task.
-*
-* @author Maxim Malnev (mailto:mazular@ya.ru)
-* @version $Id$
-* @since 0.1
-*/
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Maxim Malnev (mazular@ya.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
