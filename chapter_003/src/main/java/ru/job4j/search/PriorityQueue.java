c74d301701d285e4804c71c01e23739425e8367e
maximmalnev <mazular@ya.ru>
2018-03-15 14:02:12 +0300

    2. Очередь с приоритетом на LinkedList

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,7 +1,10 @@
 package ru.job4j.search;
+
 import java.util.LinkedList;
+
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
+
     /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определять по полю приоритет.
@@ -10,24 +13,22 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        if (tasks.size() == 0) {
-            tasks.add(task);
-        } else {
-            if (task.getPriority() > tasks.get(tasks.size() - 1).getPriority()) {
-                tasks.add(tasks.size(), task);
-            } else {
-                for (int i = 0; i < tasks.size(); i++) {
-                    if (task.getPriority() <= tasks.get(i).getPriority()) {
-                        tasks.add(i, task);
-                        break;
-                    }
-                }
+        int size = tasks.size();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (task.getPriority() <= tasks.get(i).getPriority()) {
+                tasks.add(i, task);
+                break;
             }
         }
+        if (size == tasks.size()) {
+            tasks.add(task);
+        }
     }
+
     public Task take() {
         return this.tasks.poll();
     }
+
     public void printAll() {
         for (Task task : this.tasks) {
             System.out.println(String.format("Desc: %s, Prior: %s", task.getDesc(), task.getPriority()));
