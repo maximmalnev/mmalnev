a3b42d1e07f39ceb45e2ac4532cfa0b0aabdb26d
maximmalnev <mazular@ya.ru>
2018-03-16 21:30:18 +0300

    sync

--- chapter_003/src/main/java/ru/job4j/sort/Department.java
+++ chapter_003/src/main/java/ru/job4j/sort/Department.java
@@ -1,21 +1,35 @@
 package ru.job4j.sort;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Department {
 
     public SortedSet<String> sortAscendingly(String[] stringArray) {
         SortedSet<String> sortedList = new TreeSet<>();
         for (String string : stringArray) {
-            sortedList.add(string);
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == '\\') {
+                    sortedList.add(string.substring(0, i));
+                }
+                sortedList.add(string);
+            }
         }
         return sortedList;
     }
 
-    public SortedSet<String> sortDescendingly(String[] stringArray) {
-        SortedSet<String> sortedList = new TreeSet<>();
-
+    public List<String> sortDescendingly(String[] stringArray) {
+        Set<String> sortedSet = new TreeSet<>();
+        for (String string : stringArray) {
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == '\\') {
+                    sortedSet.add(string.substring(0, i));
+                }
+                sortedSet.add(string);
+            }
+        }
+        List<String> sortedList = new ArrayList<>();
+        sortedList.addAll(sortedSet);
+        Collections.sort(sortedList, new DescComparator());
         return sortedList;
     }
 
@@ -23,6 +37,7 @@ public class Department {
         String[] depsToSort = {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
         Department department = new Department();
         System.out.println(department.sortAscendingly(depsToSort));
+        System.out.println(department.sortDescendingly(depsToSort));
     }
 }
 
