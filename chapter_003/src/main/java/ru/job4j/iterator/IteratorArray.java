03621455f74c6ed7b70f7a1f90ffa888cd9a6e87
maximmalnev <mazular@ya.ru>
2018-03-29 16:19:41 +0300

    5.1.1. Итератор для двухмерного массива int[][]

--- chapter_003/src/main/java/ru/job4j/iterator/IteratorArray.java
+++ chapter_003/src/main/java/ru/job4j/iterator/IteratorArray.java
@@ -2,6 +2,7 @@ package ru.job4j.iterator;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
 public class IteratorArray implements Iterator {
 
     private final int[][] values;
@@ -14,32 +15,25 @@ public class IteratorArray implements Iterator {
 
     @Override
     public boolean hasNext() {
-        if (values.length > posi) {
-            return values[posi].length > posj;
-        } else {
-            return false;
-        }
+        return !(values.length == posi + 1 && values[posi].length == posj);
     }
 
     @Override
     public Object next() throws NoSuchElementException {
-        int result = values[posi][posj];
-        if (values[posi].length > posj + 1) {
-            posj++;
+        if (values.length == 0) {
+            throw new NoSuchElementException();
+        } else if (posj < values[posi].length) {
+            return values[posi][posj++];
         } else {
-            posi++;
-            posj = 0;
+            do {
+                posi++;
+                posj = 0;
+            } while (values[posi].length == 0 && posi < values.length);
+            if (posi < values.length && values[posi].length != 0) {
+                return values[posi][posj++];
+            } else {
+                throw new NoSuchElementException();
+            }
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        IteratorArray ia = new IteratorArray(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9, 10, 11}});
-        while (ia.hasNext()) {
-            System.out.println(ia.next());
-        }
-        IteratorArray ia2 = new IteratorArray(new int[][]{});
-        ia2.next();
-
     }
 }
