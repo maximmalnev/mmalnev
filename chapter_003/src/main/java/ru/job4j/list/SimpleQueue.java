fb672e7832cefeec0690e55b0d72f8ab862fe7a3
maximmalnev <mazular@ya.ru>
2018-04-07 18:54:22 +0300

    sync

---
+++ chapter_003/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -0,0 +1,11 @@
+package ru.job4j.list;
+
+public class SimpleQueue<T> {
+    public <T> poll(){
+
+    }
+
+    public void push(T value){
+
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/list/SimpleStack.java
@@ -0,0 +1,11 @@
+package ru.job4j.list;
+
+public class SimpleStack<T> {
+    public <T> poll(){
+
+    }
+
+    public void push(T value){
+
+    }
+}
