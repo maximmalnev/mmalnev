2d5056b51d546555f7593fc8344f69c9f5359fe8
maximmalnev <mazular@ya.ru>
2018-04-18 15:25:41 +0300

    1. Реализовать коллекцию Set на массиве

--- chapter_003/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_003/src/main/java/ru/job4j/set/SimpleSet.java
@@ -20,19 +20,4 @@ public class SimpleSet<E> extends SimpleList<E> implements Iterable<E> {
             super.add(value);
         }
     }
-
-    public static void main(String[] args) {
-        SimpleSet<Integer> simpleSet = new SimpleSet<>(10);
-        simpleSet.add(1);
-        simpleSet.add(2);
-        simpleSet.add(2);
-        simpleSet.add(3);
-
-        System.out.println(simpleSet.get(0));
-        System.out.println(simpleSet.get(1));
-        System.out.println(simpleSet.get(2));
-        System.out.println(simpleSet.get(3));
-
-
-    }
 }
