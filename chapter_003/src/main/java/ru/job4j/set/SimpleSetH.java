2423b9a22649b97d5ea6d65ab117287478b43ba0
mmalnev <mazular@ya.ru>
2018-07-06 16:53:45 +0300

    3. Реализовать коллекцию типа Set на базе хэш-таблицы

--- chapter_003/src/main/java/ru/job4j/set/SimpleSetH.java
+++ chapter_003/src/main/java/ru/job4j/set/SimpleSetH.java
@@ -2,34 +2,30 @@ package ru.job4j.set;
 
 public class SimpleSetH<E> {
 
-    private int size, modCoefficient;
+    private boolean[] isDeleted;
     private E[] elements;
 
     public SimpleSetH(int size) {
-        this.size = size;
-        this.modCoefficient = size;
         elements = (E[]) new Object[size];
+        isDeleted = new boolean[size];
     }
 
     boolean add(E e) {
         boolean result = false;
-        int hNum = (Math.abs(e.hashCode())) % modCoefficient;
-        if (elements[hNum] == null) {
+        int hNum = (Math.abs(e.hashCode())) % elements.length;
+        if (elements[hNum] == null || isDeleted[hNum]) {
             elements[hNum] = e;
             result = true;
         } else {
-            for (int i = hNum; i < size; i++) {
-                if (elements[i] == null) {
+            for (int i = hNum; i < elements.length; i++) {
+                if (elements[i] == null || isDeleted[hNum]) {
                     elements[i] = e;
                     result = true;
                     break;
                 }
-                if (i == size - 1 && elements[i] != null) {
-                    size += 100;
-                    E[] elementsTmp = (E[]) new Object[size];
-                    System.arraycopy(elements, 0, elementsTmp, 0, elements.length);
-                    elements = elementsTmp;
-                    elements[i + 1] = e;
+                if (i == elements.length - 1 && (elements[i] != null || !isDeleted[hNum])) {
+                    expand();
+                    add(e);
                     break;
                 }
             }
@@ -37,14 +33,26 @@ public class SimpleSetH<E> {
         return result;
     }
 
+    void expand() {
+        E[] tmpElements = elements;
+        elements = (E[]) new Object[elements.length * 2];
+        boolean[] newIsDeleted = new boolean[elements.length * 2];
+        for (int i = 0; i < tmpElements.length; i++) {
+            if (tmpElements[i] != null && !isDeleted[i]) {
+                add(tmpElements[i]);
+            }
+        }
+        isDeleted = newIsDeleted;
+    }
+
     boolean contains(E e) {
         boolean result = false;
-        int hNum = (Math.abs(e.hashCode())) % modCoefficient;
+        int hNum = (Math.abs(e.hashCode())) % elements.length;
         if (elements[hNum] != null && elements[hNum].equals(e)) {
             result = true;
         } else {
-            for (int i = hNum + 1; i < size; i++) {
-                if (elements[i] == null) {
+            for (int i = hNum + 1; i < elements.length; i++) {
+                if (elements[i] == null && !isDeleted[i]) {
                     break;
                 } else if (elements[i].equals(e)) {
                     result = true;
@@ -57,46 +65,23 @@ public class SimpleSetH<E> {
 
     boolean remove(E e) {
         boolean result = false;
-        int hNum = (Math.abs(e.hashCode())) % modCoefficient;
+        int hNum = (Math.abs(e.hashCode())) % elements.length;
         if (elements[hNum] != null && elements[hNum].equals(e)) {
             elements[hNum] = null;
+            isDeleted[hNum] = true;
             result = true;
         } else {
-            for (int i = hNum + 1; i < size; i++) {
-                if (elements[i] == null) {
+            for (int i = hNum + 1; i < elements.length; i++) {
+                if (elements[i] == null && !isDeleted[i]) {
                     break;
                 } else if (elements[i].equals(e)) {
                     elements[i] = null;
+                    isDeleted[i] = true;
                     result = true;
                     break;
                 }
             }
         }
         return result;
-
     }
-
-    public static void main(String[] args) {
-        SimpleSetH simSet = new SimpleSetH<String>(4);
-
-        String string1 = "language1";
-        String string2 = "language2";
-        String string3 = "language3";
-        String string4 = "language4";
-        String string5 = "language5";
-
-
-        simSet.add(string1);
-        simSet.add(string2);
-        simSet.add(string3);
-        simSet.add(string4);
-        simSet.add(string5);
-        simSet.remove(string4);
-
-
-
-        System.out.println(simSet.contains(string4));
-        System.out.println(simSet.contains(string5));
-    }
-
 }
